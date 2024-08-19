4d70162d0c1fdf34a831285cc76d9ca3e509b81a
Alexander Rettner <alexander.rettner@web.de>
2024-08-19 19:16:47 +0200

    Switch Navbar to use Offcanvas component
    Updated the `Navbar` component to utilize the `Offcanvas` component for navigation on small screens. This change enhances the user experience by providing a slide-out menu, improving accessibility and usability.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import {Container, Nav, Navbar as BootstrapNavbar, NavDropdown} from 'react-bootstrap';
+import {Container, Nav, Navbar as BootstrapNavbar, NavDropdown, Offcanvas} from 'react-bootstrap';
 
-const Navbar = () => (
-    <BootstrapNavbar expand="lg" className="bg-body-tertiary" sticky="top">
-        <Container>
-            <BootstrapNavbar.Brand href="#home">Timesheet</BootstrapNavbar.Brand>
-            <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav"/>
-            <BootstrapNavbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/about">About</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </BootstrapNavbar.Collapse>
-        </Container>
-    </BootstrapNavbar>
-);
+const Navbar = () => {
+    return (
+        <>
+            <BootstrapNavbar expand={false} className="bg-body-tertiary" sticky="top">
+                <Container>
+                    <BootstrapNavbar.Brand href="#home">Timesheet</BootstrapNavbar.Brand>
+                    <BootstrapNavbar.Toggle aria-controls="offcanvasNavbar"/>
+                    <BootstrapNavbar.Offcanvas
+                        id="offcanvasNavbar"
+                        aria-labelledby="offcanvasNavbarLabel"
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="/about">About</Nav.Link>
+                                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                    <NavDropdown.Divider/>
+                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </BootstrapNavbar.Offcanvas>
+                </Container>
+            </BootstrapNavbar>
+        </>
+    );
+};
 
 export default Navbar;
