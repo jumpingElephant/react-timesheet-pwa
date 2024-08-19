c991c342cd303978706840f61390acbba922a689
Alexander Rettner <alexander.rettner@web.de>
2024-08-19 12:50:09 +0200

    Refactor date navigation and improve button styling
    Refactored date navigation button handlers for consistency and readability. Enhanced styling for navigation buttons to ensure responsive design across various screen sizes. Minor adjustments in JSX structure for better alignment and use of components.

--- src/App.js
+++ src/App.js
@@ -22,42 +22,39 @@ const Home = () => {
     const [currentDate, setCurrentDate] = useState(date);
 
     useEffect(() => {
-        const formattedDate = format(currentDate, 'yyyy-MM-dd');
+        const formattedDate = format(currentDate, "yyyy-MM-dd");
         navigate(`/?date=${formattedDate}`, {replace: true});
     }, [currentDate, navigate]);
 
     const handlePreviousDay = useCallback(() => {
-        setCurrentDate((prevDate) => subDays(prevDate, 1));
+        setCurrentDate(prevDate => subDays(prevDate, 1));
     }, []);
 
     const handleNextDay = useCallback(() => {
-        setCurrentDate((prevDate) => addDays(prevDate, 1));
+        setCurrentDate(prevDate => addDays(prevDate, 1));
     }, []);
 
     const handleToday = useCallback(() => {
         setCurrentDate(new Date());
     }, []);
 
-    const header = `${format(currentDate, 'E dd.MM.yyyy')} W${getWeek(currentDate)}`;
+    const header = `${format(currentDate, "E dd.MM.yyyy")} W${getWeek(currentDate)}`;
 
     return (
         <Container fluid className="align-items-center justify-content-center mw-100">
             <Row className="mb-3 App-header">
                 <Col className="d-flex justify-content-between align-items-center">
-                    <Button variant="primary" onClick={handlePreviousDay}>
+                    <Button variant="primary" className="nav-button" onClick={handlePreviousDay}>
                         <FaBackward/>
                     </Button>
-                    <div className="d-flex flex-column align-items-center mx-3">
-                        <h1 className="m-0 text-nowrap">{header}</h1>
-                        <Button
-                            variant="success"
-                            className="mt-2"
-                            onClick={handleToday}
-                        >
-                            Today
-                        </Button>
-                    </div>
-                    <Button variant="primary" onClick={handleNextDay}>
+                    <Stack className="flex-grow-1 text-center mx-3">
+                        <div className="w-100">
+                            <h1 className="m-0">{header}</h1>
+                        </div>
+                        <Row xs={4} className="justify-content-center"><Button variant="success" className="mt-2"
+                                                                               onClick={handleToday}>Today</Button></Row>
+                    </Stack>
+                    <Button variant="primary" className="nav-button" onClick={handleNextDay}>
                         <FaForward/>
                     </Button>
                 </Col>
