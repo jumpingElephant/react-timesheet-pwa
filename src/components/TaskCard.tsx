e8329454c54844e75d3fd5bbb53d889b5eef48e0
Alexander Rettner <alexander.rettner@web.de>
2024-09-20 20:23:52 +0200

    Remove react-swipeable dependency and swipe handling code
    Removed the `react-swipeable` package from `package.json` and eliminated related swipe handling logic in `TaskCard.tsx`. This simplifies the component and reduces bundle size by avoiding unnecessary dependencies.

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-import {useSwipeable} from 'react-swipeable';
+import React from 'react';
 import {Button, Card, Col, Row} from 'react-bootstrap';
 import {format} from 'date-fns';
 import {Task} from "../models/Task";
@@ -10,53 +9,10 @@ interface TaskCardProps {
     onDelete: (id: number) => void;
 }
 
-const TaskCard: React.FC<TaskCardProps> = ({task, index, onDelete}) => {
-    const [isSwiping, setIsSwiping] = useState(false);
-    const [isDeletionReady, setDeletionReady] = useState(false);
-    const [swipeDistance, setSwipeDistance] = useState(0);
-    const minSwipeDistance = 128; // Minimum swipe distance in pixels
-
-    const swipeHandlers = useSwipeable({
-        onSwiping: (eventData) => {
-            eventData.event.preventDefault(); // Prevent scrolling
-            setIsSwiping(true);
-            setSwipeDistance(eventData.deltaX);
-            setDeletionReady(Math.abs(eventData.deltaX) >= minSwipeDistance);
-        },
-        onSwiped: () => {
-            if (Math.abs(swipeDistance) >= minSwipeDistance) {
-                onDelete(task.id ?? (() => {
-                    throw new Error("Task ID is undefined");
-                })());
-            }
-            setIsSwiping(false);
-            setSwipeDistance(0);
-            setDeletionReady(false);
-        },
-        onTouchStartOrOnMouseDown: (eventData) => {
-            eventData.event.preventDefault(); // Prevent scrolling
-            setIsSwiping(true);
-        },
-        onTouchEndOrOnMouseUp: (eventData) => {
-            eventData.event.preventDefault(); // Prevent scrolling
-            setIsSwiping(false);
-            setSwipeDistance(0);
-            setDeletionReady(false);
-        },
-        preventScrollOnSwipe: true,
-        trackMouse: false,
-        swipeDuration: 2000,
-        delta: minSwipeDistance,
-    });
+const TaskCard: React.FC<TaskCardProps> = ({task, onDelete}) => {
 
     return (
-        <Card
-            //{...swipeHandlers}
-            className={`mb-1 card-hover border-hover ${isDeletionReady ? 'bg-danger' : index % 2 === 0 ? 'bg-light' : ''} ${
-                isSwiping ? 'swiping' : ''
-            }`}
-            style={{transform: `translateX(${swipeDistance}px)`}}
-        >
+        <Card className={`mb-1 card-hover border-hover`}>
             <Card.Body>
                 <Card.Title>{task.title}</Card.Title>
                 <Row className="align-items-center">
